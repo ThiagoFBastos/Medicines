40085e7f31a0085ec68d208c8afc8ed67f00de5a
Thiago Bastos <thiagobastos1705@gmail.com>
2026-04-18 19:28:14 -0300

    refactor: move the regex business to your own service

---
+++ Medicines/Commands/UpdateCommand.cs
@@ -0,0 +1,11 @@
+﻿using Medicines.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Medicines.Commands
+{
+    public record UpdateCommand(string Medicine, int PillsQuantity, int Hours, int Minutes): Command;
+}
